a4ffeefbc6ceebea91e9fed156c9313196d158b6
Priyal Parikh <priyal.p.1992@gmail.com>
2017-05-11 03:10:14 -0400

    upload ready

--- js/main.js
+++ js/main.js
@@ -1,130 +1,320 @@
-function position(){
-    if(window.pageYOffset >= ($('#home').height()+60)){
-        $('#sidebar').css('position', 'fixed');
-        $('#sidebar').css('top', '0');
-        $('#sidebar').css('left', '88vw');
-    }
-    else{
-        $('#sidebar').css('position', 'relative');
-        $('#sidebar').css('top', '0');
-        $('#sidebar').css('left', '0'); 
+function showmenu(){
+        $('.mobile-view').toggle();
+        $('.bar').toggleClass('animate');
+}
+
+function showcontent(e){
+    var opened = e.target;
+    $('.tab-content > ul').hide(100);
+    $('.tab').removeClass('clicked');
+    $(opened).addClass('clicked');
+
+    switch(opened){
+    case $('.tab')[0]:
+        $('#me').addClass('clicked');
+        $('#mesh').show(200); 
+        break;
+    case $('#backoptions > span')[0]:
+        $('#me').addClass('clicked');
+        $('#mesh').show(200); 
+        break;
+    case $('.tab')[1]:
+        $('#el').addClass('clicked');
+        $('#elastic').show(200);
+        break;
+    case $('#backoptions > span')[2]:
+        $('#el').addClass('clicked');
+        $('#elastic').show(200);
+        break;
+    case $('.tab')[2]:
+        $('#fa').addClass('clicked');
+        $('#fabric').show(200);
+        break;
+    case $('#backoptions > span')[1]:
+        $('#fa').addClass('clicked');
+        $('#fabric').show(200);
+        break;
+    case $('.tab')[3]:
+        $('#zi').addClass('clicked');
+        $('#zipper').show(200);
+        break;
+    case $('#backoptions > span')[3]:
+        $('#zi').addClass('clicked');
+        $('#zipper').show(200);
+        break;
     }
 }
 
-function gridcolors(){
-    $('#meshup').css('margin-left', '0');
-    $('#stretch').css('margin-left', '-1000px');
-    $('#fab').css('margin-left', '-1000px');
-}
-function fabriccolors(){
-    $('#meshup').css('margin-left', '-1000px');
-    $('#stretch').css('margin-left', '-1000px');
-    $('#fab').css('margin-left', '0');
-}
-function elasticcolors(){
-    $('#meshup').css('margin-left', '-1000px');
-    $('#stretch').css('margin-left', '0');
-    $('#fab').css('margin-left', '-1000px');
-}
 
+function buildgrid(f){
+    var clicked = f.target;
 
-function buildgrid(e){
-    var clicked = e.target;
-    if(clicked === $('#mesh > li > span')[0]){
-        console.log('clicked');
-        $('#grid').html('<img src="img/buildback/backgridblack.png">');
+    switch(clicked){
+    case $('#mesh > li > span')[0]:
+        $('#gridback').css('background-image', 'url("img/buildback/backgridblack.png")');
+        $('#gridside').css('background-image', 'url("img/buildside/sidegridblack.png")');
+        $('#mv').html('0.00');
+        break;
+    case $('#mesh > li > span')[1]:
+        $('#gridback').css('background-image', 'url("img/buildback/backgridwhite.png")');
+        $('#gridside').css('background-image', 'url("img/buildside/sidegridwhite.png")');
+        $('#mv').html('20.00');
+        break;
+    case $('#mesh > li > span')[2]:
+        $('#gridback').css('background-image', 'url("img/buildback/backgridblue.png")');
+        $('#gridside').css('background-image', 'url("img/buildside/sidegridblue.png")');
+        $('#mv').html('15.00');
+        break;
+    case $('#mesh > li > span')[3]:
+        $('#gridback').css('background-image', 'url("img/buildback/backgridred.png")');
+        $('#gridside').css('background-image', 'url("img/buildside/sidegridred.png")');
+        $('#mv').html('30.00');
+        break;
+    case $('#mesh > li > span')[4]:
+        $('#gridback').css('background-image', 'url("img/buildback/backgridgray.png")');
+        $('#gridside').css('background-image', 'url("img/buildside/sidegridgray.png")');
+        $('#mv').html('20.00');
+        break;
     }
-    else if(clicked === $('#mesh > li > span')[1]){
-        $('#grid').html('<img src="img/buildback/backgridwhite.png">');
-    }
-    else if(clicked === $('#mesh > li > span')[2]){
-        $('#grid').html('<img src="img/buildback/backgridblue.png">');
-    }
-    else if(clicked === $('#mesh > li > span')[3]){
-        $('#grid').html('<img src="img/buildback/backgridred.png">');
-    }
-    else if(clicked === $('#mesh > li > span')[4]){
-        $('#grid').html('<img src="img/buildback/backgridgray.png">');
-    }
+    totalvalue();
 }
 
 function buildelastic(e){
     var clicked = e.target;
-    if(clicked === $('#elastic > li > span')[0]){
-        $('#tb').html('<img src="img/buildback/topbandblack.png">');
-        $('#mb').html('<img src="img/buildback/middlebandblack.png">');
-        $('#bb').html('<img src="img/buildback/bottombandblack.png">');
+
+    switch(clicked){
+    case $('#elastic > li > span')[0]:
+        $('#bandsback').css('background-image', 'url("img/buildback/backbandsblack.png")');
+        $('#bandsfront').css('background-image', 'url("img/buildfront/frontbandsblack.png")');
+        $('#bandsside').css('background-image', 'url("img/buildside/sidebandsblack.png")');
+        $('#ev').html('0.00');
+        break;
+    case $('#elastic > li > span')[1]:
+        $('#bandsback').css('background-image', 'url("img/buildback/backbandswhite.png")');
+        $('#bandsfront').css('background-image', 'url("img/buildfront/frontbandswhite.png")');
+        $('#bandsside').css('background-image', 'url("img/buildside/sidebandswhite.png")');
+        $('#ev').html('15.00');
+        break;
+    case $('#elastic > li > span')[2]:
+        $('#bandsback').css('background-image', 'url("img/buildback/backbandsblue.png")');
+        $('#bandsfront').css('background-image', 'url("img/buildfront/frontbandsblue.png")');
+        $('#bandsside').css('background-image', 'url("img/buildside/sidebandsblue.png")');
+        $('#ev').html('15.00');
+        break;
+    case $('#elastic > li > span')[3]:
+        $('#bandsback').css('background-image', 'url("img/buildback/backbandsred.png")');
+        $('#bandsfront').css('background-image', 'url("img/buildfront/frontbandsred.png")');
+        $('#bandsside').css('background-image', 'url("img/buildside/sidebandsred.png")');
+        $('#ev').html('30.00');
+        break;
+    case $('#elastic > li > span')[4]:
+        $('#bandsback').css('background-image', 'url("img/buildback/backbandsgray.png")');
+        $('#bandsfront').css('background-image', 'url("img/buildfront/frontbandsgray.png")');
+        $('#bandsside').css('background-image', 'url("img/buildside/sidebandsgray.png")');
+        $('#ev').html('20.00');
+        break;
     }
-    else if(clicked === $('#elastic > li > span')[1]){
-        $('#tb').html('<img src="img/buildback/topbandwhite.png">');
-        $('#mb').html('<img src="img/buildback/middlebandwhite.png">');
-        $('#bb').html('<img src="img/buildback/bottombandwhite.png">');
-    }
-    else if(clicked === $('#elastic > li > span')[2]){
-        $('#tb').html('<img src="img/buildback/topbandblue.png">');
-        $('#mb').html('<img src="img/buildback/middlebandblue.png">');
-        $('#bb').html('<img src="img/buildback/bottombandblue.png">');
-    }
-    else if(clicked === $('#elastic > li > span')[3]){
-        $('#tb').html('<img src="img/buildback/topbandred.png">');
-        $('#mb').html('<img src="img/buildback/middlebandred.png">');
-        $('#bb').html('<img src="img/buildback/bottombandred.png">');
-    }
-    else if(clicked === $('#elastic > li > span')[4]){
-        $('#tb').html('<img src="img/buildback/topbandgray.png">');
-        $('#mb').html('<img src="img/buildback/middlebandgray.png">');
-        $('#bb').html('<img src="img/buildback/bottombandgray.png">');
-    }
+    totalvalue();
 }
 
 function buildfabric(e){
     var clicked = e.target;
-    if(clicked === $('#fabric > li > span')[0]){
-        $('#lp').html('<img src="img/buildback/leftpatchblack.png">');
-        $('#rp').html('<img src="img/buildback/rightpatchblack.png">');
+
+    switch(clicked){
+    case $('#fabric > li > span')[0]:
+        $('#fabricback').css('background-image', 'url("img/buildback/backfabricblack.png")');
+        $('#fabricfront').css('background-image', 'url("img/buildfront/frontfabricblack.png")');
+        $('#fabricside').css('background-image', 'url("img/buildside/sidefabricblack.png")');
+        $('#fv').html('0.00');
+        break;
+    case $('#fabric > li > span')[1]:
+        $('#fabricback').css('background-image', 'url("img/buildback/backfabricwhite.png")');
+        $('#fabricfront').css('background-image', 'url("img/buildfront/frontfabricwhite.png")');
+        $('#fabricside').css('background-image', 'url("img/buildside/sidefabricwhite.png")');
+        $('#fv').html('15.00');
+        break;
+    case $('#fabric > li > span')[2]:
+        $('#fabricback').css('background-image', 'url("img/buildback/backfabricblue.png")');
+        $('#fabricfront').css('background-image', 'url("img/buildfront/frontfabricblue.png")');
+        $('#fabricside').css('background-image', 'url("img/buildside/sidefabricblue.png")');
+        $('#fv').html('20.00');
+        break;
+    case $('#fabric > li > span')[3]:
+        $('#fabricback').css('background-image', 'url("img/buildback/backfabricred.png")');
+        $('#fabricfront').css('background-image', 'url("img/buildfront/frontfabricred.png")');
+        $('#fabricside').css('background-image', 'url("img/buildside/sidefabricred.png")');
+        $('#fv').html('40.00');
+        break;
+    case $('#fabric > li > span')[4]:
+        $('#fabricback').css('background-image', 'url("img/buildback/backfabricgray.png")');
+        $('#fabricfront').css('background-image', 'url("img/buildfront/frontfabricgray.png")');
+        $('#fabricside').css('background-image', 'url("img/buildside/sidefabricgray.png")');
+        $('#fv').html('10.00');
+        break;
     }
-    else if(clicked === $('#fabric > li > span')[1]){
-        $('#lp').html('<img src="img/buildback/leftpatchwhite.png">');
-        $('#rp').html('<img src="img/buildback/rightpatchwhite.png">');
+    totalvalue();
+}
+
+function buildzipper(e){
+    var clicked = e.target;
+    if(clicked === $('#zipper > li > span')[0]){
+        $('#zipperfront').css('background-image', 'url("img/buildfront/frontzipperblack.png")');
+        $('#zv').html('0.00');
     }
-    else if(clicked === $('#fabric > li > span')[2]){
-        $('#lp').html('<img src="img/buildback/leftpatchblue.png">');
-        $('#rp').html('<img src="img/buildback/rightpatchblue.png">');
+    else if(clicked === $('#zipper > li > span')[1]){
+        $('#zipperfront').css('background-image', 'url("img/buildfront/frontzipperwhite.png")');
+        $('#zv').html('15.00');
     }
-    else if(clicked === $('#fabric > li > span')[3]){
-        $('#lp').html('<img src="img/buildback/leftpatchred.png">');
-        $('#rp').html('<img src="img/buildback/rightpatchred.png">');
+    else if(clicked === $('#zipper > li > span')[2]){
+        $('#zipperfront').css('background-image', 'url("img/buildfront/frontzipperblue.png")');
+        $('#zv').html('20.00');
     }
-    else if(clicked === $('#fabric > li > span')[4]){
-        $('#lp').html('<img src="img/buildback/leftpatchgray.png">');
-        $('#rp').html('<img src="img/buildback/rightpatchgray.png">');
+    else if(clicked === $('#zipper > li > span')[3]){
+        $('#zipperfront').css('background-image', 'url("img/buildfront/frontzipperred.png")');
+        $('#zv').html('40.00');
     }
+    else if(clicked === $('#zipper > li > span')[4]){
+        $('#zipperfront').css('background-image', 'url("img/buildfront/frontzippergray.png")');
+        $('#zv').html('10.00');
+    }
+    totalvalue();
 }
 
 function scrollbuild(e){
     var clicked = e.target;
-    if(clicked == $('.bullet')[0]){
+
+    switch(clicked){
+    case $('.bullet')[0]:
         $('#scroll').css('margin-left', '0');
+        break;
+    case $('.bullet')[1]:
+        $('#scroll').css('margin-left', -1*$('#scroll').width()/3 + 'px');
+        break;
+    case $('.bullet')[2]:
+        $('#scroll').css('margin-left', -2*$('#scroll').width()/3 + 'px');
+        break;
     }
-    else if(clicked == $('.bullet')[1]){
-        $('#scroll').css('margin-left', '-510px');
+}
+
+function totalvalue(){
+    var sum = 130.00;
+    sum += parseFloat($('#mv').html());
+    sum += parseFloat($('#ev').html());
+    sum += parseFloat($('#fv').html());
+    sum += parseFloat($('#zv').html());
+    $('#total > .value').html('$'+ sum + '.00');
+}
+
+var d = new Date();
+var year = d.getFullYear(),
+    month = d.getMonth(),
+    date = d.getDate(),
+    // day = d.getDay(),
+    firstDay = new Date(d.getFullYear(), d.getMonth(), 1).getDay(),
+    lastDay = new Date((d.getMonth() + 1 > 11) ? d.getFullYear() + 1 : d.getFullYear(), (d.getMonth() + 1 > 12) ? 0 : d.getMonth() + 1, 0).getDate(),
+    months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+    days = ['Sunday', 'Monday', 'Tuesday', 'Thursday', 'Friday', 'Saturday'];
+
+var theRows = [], theDays = [], theRadios = [];
+
+function calendar(){
+    $('.year > span').html(year);
+    $('.month > span').html(months[month]);
+
+
+    for (var i = 0; i < 6; i++) {
+        theRows[i] = document.createElement('DIV');
+        theRows[i].className = 'row';
+        $('.days').append(theRows[i]);
     }
-    if(clicked == $('.bullet')[2]){
-        $('#scroll').css('margin-left', '-1020px');
+
+    for (var i = 0, j = 0; i < 42; i++) {
+        theRadios[i] = document.createElement('INPUT');
+        theRadios[i].className = 'radios';
+        theRadios[i].type = 'radio';
+        theRadios[i].name = 'radios';
+        theRadios[i].id = 'radio-' + (i + 1);
+
+        theDays[i] = document.createElement('LABEL');
+        theDays[i].className = 'day';
+        theDays[i].htmlFor = 'radio-' + (i + 1);
+        theDays[i].id = 'day-' + (i + 1);
+
+        theRows[j].appendChild(theRadios[i]);
+        theRows[j].appendChild(theDays[i]);
+
+        if ((i + 1) % 7 == 0)
+            j++;
     }
+
+    var counter = 1;
+    for(var i = firstDay; i < lastDay+firstDay; i++){
+        theDays[i].innerHTML = (counter);
+        counter++;
+    }
+
+    $('#radio-'+ (date + firstDay)).prop('checked', true);
+
+    $('.events > .date > span').html(d.getDate() + ' ' + months[d.getMonth()] + ', ' + d.getFullYear());
+}
+
+function calendarupdate(e){
+    for(var i = firstDay; i < lastDay+firstDay; i++){
+        theDays[i].innerHTML = '';
+    }
+    var change = e.target;
+    // console.log(change)
+    switch(change){
+    case $('.year > div')[0]:
+        year--;
+        $('.year > span').html(year);
+        break;
+    case $('.year > div')[1]:
+        year++;
+        $('.year > span').html(year);
+        break;
+    case $('.month > div')[0]:
+        month--;
+        $('.month > span').html(months[month]);
+        break;
+    case $('.month > div')[1]:
+        month++;
+        $('.month > span').html(months[month]);
+        break;
+    }
+    
+    var ddate = new Date(year, month);
+    firstDay = new Date(ddate.getFullYear(), ddate.getMonth(), 1).getDay();
+    lastDay = new Date((ddate.getMonth() + 1 > 11) ? ddate.getFullYear() + 1 : ddate.getFullYear(), (ddate.getMonth() + 1 > 12) ? 0 : ddate.getMonth() + 1, 0).getDate();
+
+    var counter = 1;
+    for(var i = firstDay; i < lastDay+firstDay; i++){
+        theDays[i].innerHTML = (counter);
+        counter++;
+    }
+    var labelfor = $('.days > .row > input[type=\'radio\']:checked').attr('id'),
+        eventdate = $('.days > .row > label[for=\'' + labelfor + '\']').text();
+    $('.events > .date > span').html(eventdate + ' ' + months[ddate.getMonth()] + ', ' + ddate.getFullYear());
 }
 
 $(window).load(function(){
     'use strict';
-    $(window).scroll(position);
 
     $('.bullet').click(scrollbuild);
 
-    $('#backoptions > .grid').click(gridcolors);
-    $('#backoptions > .fabric').click(fabriccolors);
-    $('#backoptions > .elastic').click(elasticcolors);
+    $('.hamburger-menu').click(showmenu);
 
-    $('#mesh > li').click(buildgrid);
-    $('#elastic > li').click(buildelastic);
-    $('#fabric > li').click(buildfabric);
+    $('.tab-content > ul').hide();
+    $('#tabs').click(showcontent);
+    $('#backoptions > span').click(showcontent);
+
+    $('#mesh > li > span').click(buildgrid);
+    $('#elastic > li > span').click(buildelastic);
+    $('#fabric > li > span').click(buildfabric);
+    $('#zipper > li > span').click(buildzipper);
+
+    calendar();
+    $('.year > div').click(calendarupdate);
+    $('.month > div').click(calendarupdate);
+    $('.days > .row > input:radio').click(calendarupdate);
 });
